65f1432c4506d450cc2359d5e1b8066846ee26f5
Kuttivel-Dharmaraj <kuttiveldharmaraj@gmail.com>
2026-02-02 00:15:03 +0000

    Database connection successful. MongoDB as usual

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDataBase } from "./config/database.js";
+
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
+
+connectDataBase();
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
